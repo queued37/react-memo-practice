e8b20d3fe6ed8ed5beee2a50f78a36da2f252974
queued37 <queued37@gmail.com>
2018-05-10 19:04:56 +0900

    Add sign in and logout API

--- server/routes/account.js
+++ server/routes/account.js
@@ -56,16 +56,77 @@ router.post('/sign-up', (req, res) => {
   })
 })
 
+/**
+ * Account sign in
+ * POST /api/account/sign-in
+ * Body example: { "username": "test", "password": "test" }
+ * Error codes:
+ *   1: Login failed
+ */
 router.post('/sign-in', (req, res) => {
-  res.json({ success: true })
+  if (typeof req.body.password !== 'string') {
+    return res.status(400).json({
+      error: 'Login failed',
+      code: 1
+    })
+  }
+
+  Account.findOne({ username: req.body.username }, (err, account) => {
+    if (err) throw err
+
+    // Check account existence
+    if (!account) {
+      return res.status(401).json({
+        error: 'Login failed',
+        code: 1
+      })
+    }
+
+    // Validate password
+    if (!account.validateHash(req.body.password)) {
+      return res.status(401).json({
+        error: 'Login failed',
+        code: 1
+      })
+    }
+
+    // Alter session
+    let session = req.session
+    session.loginInfo = {
+      _id: account._id,
+      username: account.username
+    }
+
+    return res.json({
+      success: true
+    })
+  })
 })
 
+/**
+ * Get session info
+ * GET /api/account/get-info
+ * Error codes:
+ *   1: Not logged in
+ */
 router.get('/get-info', (req, res) => {
-  res.json({ info: null })
+  console.log(req.session)
+  if (typeof req.session.loginInfo === 'undefined') {
+    return res.status(401).json({
+      error: 1
+    })
+  }
+
+  res.json({ info: req.session.loginInfo })
 })
 
+/**
+ * Account logout
+ * POST /api/account/logout
+ */
 router.post('/logout', (req, res) => {
-  return res.json({ success: true })
+  req.session.destroy(err => { if (err) throw err })
+  return res.json({ sucess: true })
 })
 
 export default router
